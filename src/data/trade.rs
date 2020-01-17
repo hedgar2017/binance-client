0be2a0b65685cb1ff768597e850221129f641f11
hedgar2017 <hedgar2017@gmail.com>
2020-01-17 22:54:03 +0200

    Make all the WebSocket trade event fields public

--- src/data/trade.rs
+++ src/data/trade.rs
@@ -8,37 +8,27 @@ use serde_derive::Deserialize;
 #[derive(Debug, Deserialize)]
 pub struct Trade {
     #[serde(rename = "e")]
-    event_type: String,
+    pub event_type: String,
     #[serde(rename = "E")]
-    event_time: i64,
+    pub event_time: i64,
     #[serde(rename = "s")]
-    symbol: String,
+    pub symbol: String,
     #[serde(rename = "t")]
-    trade_id: i64,
+    pub trade_id: i64,
     #[serde(rename = "p")]
     #[serde(deserialize_with = "crate::data::serde::deserialize_decimal")]
-    price: Decimal,
+    pub price: Decimal,
     #[serde(rename = "q")]
     #[serde(deserialize_with = "crate::data::serde::deserialize_decimal")]
-    quantity: Decimal,
+    pub quantity: Decimal,
     #[serde(rename = "b")]
-    buyer_order_id: i64,
+    pub buyer_order_id: i64,
     #[serde(rename = "a")]
-    seller_order_id: i64,
+    pub seller_order_id: i64,
     #[serde(rename = "T")]
-    trade_time: i64,
+    pub trade_time: i64,
     #[serde(rename = "m")]
-    is_market_maker: bool,
+    pub is_market_maker: bool,
     #[serde(rename = "M")]
-    ignore: bool,
-}
-
-impl Trade {
-    pub fn price(&self) -> Decimal {
-        self.price
-    }
-
-    pub fn quantity(&self) -> Decimal {
-        self.quantity
-    }
+    pub ignore: bool,
 }
