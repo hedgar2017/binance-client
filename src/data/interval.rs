4c6dfe86708327d2241d579eaf68e1717afb048c
hedgar2017 <hedgar2017@gmail.com>
2021-01-18 11:17:26 +0200

    Improve the interval entity, add the H12 interval

--- src/data/interval.rs
+++ src/data/interval.rs
@@ -2,6 +2,7 @@
 //! The interval.
 //!
 
+use std::convert::TryFrom;
 use std::fmt;
 
 use serde::Deserialize;
@@ -28,62 +29,81 @@ pub enum Interval {
     #[serde(rename = "m30")]
     Minute30,
     /// The 1 hour interval.
-    #[serde(rename = "h1")]
+    #[serde(rename = "H1")]
     Hour1,
     /// The 2 hours interval.
-    #[serde(rename = "h2")]
+    #[serde(rename = "H2")]
     Hour2,
     /// The 4 hours interval.
-    #[serde(rename = "h4")]
+    #[serde(rename = "H4")]
     Hour4,
     /// The 6 hours interval.
-    #[serde(rename = "h6")]
+    #[serde(rename = "H6")]
     Hour6,
     /// The 8 hours interval.
-    #[serde(rename = "h8")]
+    #[serde(rename = "H8")]
     Hour8,
+    /// The 12 hours interval.
+    #[serde(rename = "H12")]
+    Hour12,
     /// The 1 day interval.
-    #[serde(rename = "d1")]
+    #[serde(rename = "D1")]
     Day1,
     /// The 3 days interval.
-    #[serde(rename = "d3")]
+    #[serde(rename = "D3")]
     Day3,
     /// The 1 week interval.
-    #[serde(rename = "w1")]
+    #[serde(rename = "W1")]
     Week1,
     /// The 1 month interval.
     #[serde(rename = "M1")]
     Month1,
 }
 
-impl Interval {
-    /// Helper constant for calculating time intervals.
-    const SECONDS_IN_A_MINUTE: i64 = 60;
-    /// Helper constant for calculating time intervals.
-    const SECONDS_IN_AN_HOUR: i64 = Self::SECONDS_IN_A_MINUTE * 60;
-    /// Helper constant for calculating time intervals.
-    const SECONDS_IN_A_DAY: i64 = Self::SECONDS_IN_AN_HOUR * 24;
-
-    ///
-    /// The interval duration in seconds.
-    ///
-    pub fn secs(self) -> i64 {
+impl Into<i64> for Interval {
+    fn into(self) -> i64 {
         match self {
-            Interval::Minute1 => Self::SECONDS_IN_A_MINUTE,
-            Interval::Minute3 => Self::SECONDS_IN_A_MINUTE * 3,
-            Interval::Minute5 => Self::SECONDS_IN_A_MINUTE * 5,
-            Interval::Minute15 => Self::SECONDS_IN_A_MINUTE * 15,
-            Interval::Minute30 => Self::SECONDS_IN_A_MINUTE * 30,
-            Interval::Hour1 => Self::SECONDS_IN_AN_HOUR,
-            Interval::Hour2 => Self::SECONDS_IN_AN_HOUR * 2,
-            Interval::Hour4 => Self::SECONDS_IN_AN_HOUR * 4,
-            Interval::Hour6 => Self::SECONDS_IN_AN_HOUR * 6,
-            Interval::Hour8 => Self::SECONDS_IN_AN_HOUR * 8,
-            Interval::Day1 => Self::SECONDS_IN_A_DAY,
-            Interval::Day3 => Self::SECONDS_IN_A_DAY * 3,
-            Interval::Week1 => Self::SECONDS_IN_A_DAY * 7,
-            Interval::Month1 => Self::SECONDS_IN_A_DAY * 30,
+            Interval::Minute1 => 60,
+            Interval::Minute3 => 180,
+            Interval::Minute5 => 300,
+            Interval::Minute15 => 900,
+            Interval::Minute30 => 1800,
+            Interval::Hour1 => 3600,
+            Interval::Hour2 => 7200,
+            Interval::Hour4 => 14400,
+            Interval::Hour6 => 21600,
+            Interval::Hour8 => 28800,
+            Interval::Hour12 => 43200,
+            Interval::Day1 => 86400,
+            Interval::Day3 => 259200,
+            Interval::Week1 => 604800,
+            Interval::Month1 => 2592000,
         }
+    }
+}
+
+impl TryFrom<i64> for Interval {
+    type Error = i64;
+
+    fn try_from(value: i64) -> Result<Self, Self::Error> {
+        Ok(match value {
+            60 => Self::Minute1,
+            180 => Self::Minute3,
+            300 => Self::Minute5,
+            900 => Self::Minute15,
+            1800 => Self::Minute30,
+            3600 => Self::Hour1,
+            7200 => Self::Hour2,
+            14400 => Self::Hour4,
+            21600 => Self::Hour6,
+            28800 => Self::Hour8,
+            43200 => Self::Hour12,
+            86400 => Self::Day1,
+            259200 => Self::Day3,
+            604800 => Self::Week1,
+            2592000 => Self::Month1,
+            value => return Err(value),
+        })
     }
 }
 
@@ -93,20 +113,21 @@ impl fmt::Display for Interval {
             f,
             "{}",
             match self {
-                Interval::Minute1 => "1m",
-                Interval::Minute3 => "3m",
-                Interval::Minute5 => "5m",
-                Interval::Minute15 => "15m",
-                Interval::Minute30 => "30m",
-                Interval::Hour1 => "1h",
-                Interval::Hour2 => "2h",
-                Interval::Hour4 => "4h",
-                Interval::Hour6 => "6h",
-                Interval::Hour8 => "8h",
-                Interval::Day1 => "1d",
-                Interval::Day3 => "3d",
-                Interval::Week1 => "1w",
-                Interval::Month1 => "1M",
+                Interval::Minute1 => "m1",
+                Interval::Minute3 => "m3",
+                Interval::Minute5 => "m5",
+                Interval::Minute15 => "m15",
+                Interval::Minute30 => "m30",
+                Interval::Hour1 => "H1",
+                Interval::Hour2 => "H2",
+                Interval::Hour4 => "H4",
+                Interval::Hour6 => "H6",
+                Interval::Hour8 => "H8",
+                Interval::Hour12 => "H12",
+                Interval::Day1 => "D1",
+                Interval::Day3 => "D3",
+                Interval::Week1 => "W1",
+                Interval::Month1 => "M1",
             }
         )
     }
